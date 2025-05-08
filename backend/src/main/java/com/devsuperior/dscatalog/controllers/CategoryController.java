808e03eb4919edc7c5c13a6c3d21d90634eaec9f
Felipe Lorenzon <srskru@gmail.com>
2025-05-08 10:23:59 -0300

    Pagenation

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,8 @@ public class CategoryController {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll() {
-		List<CategoryDTO> list = service.findAll();
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok(list);
 	}
 	
